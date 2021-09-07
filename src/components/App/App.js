de8ebd01843c305602f843c9bf41905469a92734
aureliennezzar <aurelien.nezzar@outlook.fr>
2021-09-07 11:48:28 +0200

    paris event init

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Paris Events</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from 'components/App/App';
 import reportWebVitals from './reportWebVitals';
+import {
+    BrowserRouter as Router,
+} from "react-router-dom"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Router>
+        <App/>
+    </Router>
+
+    , document.querySelector('#root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
